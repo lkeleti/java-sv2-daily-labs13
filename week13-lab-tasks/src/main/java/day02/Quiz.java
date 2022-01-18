6b255bb0d32d5d9ebb85a6c08fa350cbef743d76
lkeleti <keleti.kaszlo@vipmail.hu>
2022-01-18 20:17:25 +0100

    week13 day02 exercise completed

--- week13-lab-tasks/src/main/java/day02/Quiz.java
+++ week13-lab-tasks/src/main/java/day02/Quiz.java
@@ -55,9 +55,34 @@ public class Quiz {
         return actual[numberOfQuestion] == goodAnswer[numberOfQuestion];
     }
 
+    public String whoWasTheBest() {
+        int maxPoint = 0;
+        String bestId = "";
+
+        for (Map.Entry<String, char[]> e : answers.entrySet()) {
+            int actualPoint = 0;
+            for (int i = 0; i < 5; i++) {
+                char actualValue = e.getValue()[i];
+                if (actualValue != '!') {
+                    if (actualValue == goodAnswer[i]) {
+                        actualPoint += i + 1;
+                    } else if (actualValue != 'X') {
+                        actualPoint -= 2;
+                    }
+                }
+            }
+            if (actualPoint > maxPoint) {
+                maxPoint = actualPoint;
+                bestId = e.getKey();
+            }
+        }
+        return bestId;
+    }
+
     public static void main(String[] args) {
         Quiz quiz = new Quiz("src/main/resources/result.txt");
         System.out.println(quiz.isCorrectAnswerById("AB123", 0));
         System.out.println(quiz.isCorrectAnswerById("AH2", 0));
+        System.out.println(quiz.whoWasTheBest());
     }
 }
